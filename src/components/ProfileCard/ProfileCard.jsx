9aa216594d191ca0f62b21377bc75094355d88ee
Bodhisatwa2002 <bodhisatwachakraborty2002@gmail.com>
2023-06-18 10:56:47 +0530

    adding Followers Card

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './ProfileCard.css'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+        <div className="ProfileImages">
+            <img src={Cover} alt="" />
+            <img src={Profile} alt="" />
+        </div>
+
+        <div className="ProfileName">
+            <span>
+                Pratistha Chakraborty
+            </span>
+            <span>
+                Senior FrontEnd Developer
+            </span>
+        </div>
+
+        <div className="followStatus">
+            <hr/>
+            <div>
+                <div className="follow">
+                    <span>19,000</span>
+                    <span>followers</span>
+                </div>
+                <div className="vl"></div>
+                <div className="follow">
+                    <span>19</span>
+                    <span>followings</span>
+                </div>
+            </div>
+            <hr/>
+        </div>
+
+        <span>
+            My Profile
+        </span>
+    </div>
+  )
+}
+
+export default ProfileCard
